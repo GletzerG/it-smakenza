b97849365df4c7a9bdd038c48f9735573c087bea
razendra17 <daffaevandrez25@gmail.com>
2026-04-15 18:33:56 +0800

    merged 1

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,10 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-<<<<<<< HEAD
-=======
 Route::get('/', function () {
     return view('pages.welcome.index');
 });
 
->>>>>>> 9dfe1cf41a28c23c7195459a2e212a3531d9c3ab
 Route::get('/dashboard', function () {
     return view('pages.dashboard.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
